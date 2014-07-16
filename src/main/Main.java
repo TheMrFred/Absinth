ed2d8645c1c676ea4d930a90de0784dd27aca6cf
TheMrFred <fredericrousseau23@gmail.com>
2014-07-15 22:59:20 -0700

    Removed commentaries.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		// Non ta mère
-=======
-		// Un commentaire.
->>>>>>> origin/master
 	}
 
 }
